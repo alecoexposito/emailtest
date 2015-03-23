216536a5a7ee7c51425221478e65df1a729f6974
alex <alecoexposito@gmail.com>
2015-03-23 11:49:44 -0400

    changes

--- src/main/java/emailtest/classes/Main.java
+++ src/main/java/emailtest/classes/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Sending email...");
 		TestEmail testEmail = new TestEmail();
-		testEmail.sendEmail("alecoexposito@gmail.com");
+		testEmail.sendEmail("hamlethabana84@gmail.com");
 		System.out.println("Email sent");
 
 	}
