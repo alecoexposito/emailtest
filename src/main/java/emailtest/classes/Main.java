1a0fefb0278bc887062db95a8433dbedb79aced0
alex <alecoexposito@gmail.com>
2015-03-23 10:11:00 -0400

    changes

--- src/main/java/emailtest/classes/Main.java
+++ src/main/java/emailtest/classes/Main.java
@@ -3,7 +3,7 @@ package emailtest.classes;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Sending email");
+		System.out.println("Sending email...");
 		TestEmail testEmail = new TestEmail();
 		testEmail.sendEmail("alecoexposito@gmail.com");
 		System.out.println("Email sent");
